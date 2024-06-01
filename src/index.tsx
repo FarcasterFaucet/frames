6e94f19c602f45e011d79ef3da8233fb378e01f1
Fabrizio Vigevani <fabriziovigevani@Fabrizios-MacBook-Pro.local>
2024-06-01 02:45:56 +0200

    Update logic and add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,82 +1,85 @@
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { serveStatic } from 'frog/serve-static'
+import { Button, Frog, TextInput } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
 
-import { isUserRegistered, registerUser } from './lib/faucet.js'
+import { getUserStatus, registerUser } from "./lib/faucet.js";
+import { Box } from "./components/index.js";
 
 export const app = new Frog({
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
-})
+});
 
-app.frame('/', (c) => {
+app.frame("/", (c) => {
   // Fetch if user is registered
 
   return c.res({
-    action: '/registerOrClaim',
+    action: "/checkClaim",
     image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 120 }}>
-          Faucet
+      <div style={{ color: "white", display: "flex", fontSize: 120 }}>
+        Faucet
       </div>
     ),
-    intents: [
-      <Button>Check claimability!</Button>
-    ],
-  })
-})
+    intents: [<Button>Check claimability!</Button>],
+  });
+});
 
-
-app.frame('/registerOrClaim', (c) => {
+app.frame("/checkClaim", async (c) => {
   // Fetch if user is registered
-  const { fid } = c.frameData || {}
+  const { fid } = c.frameData || {};
   if (!fid) {
-    return  c.error({ message: 'No fid'})// TODO: update message error 
+    return c.error({ message: "No fid" }); // TODO: update message error
   }
 
-  const {} = isUserRegistered(fid)
+  const { canClaim, registered } = await getUserStatus(fid);
 
   return c.res({
+    action: canClaim ? "/claimed" : "/registered",
     image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 120 }}>
-          Faucet
+      <div style={{ color: "white", display: "flex", fontSize: 120 }}>
+        {canClaim
+          ? "You can now claim!"
+          : registered
+          ? "You can claim on..."
+          : "Register for next period"}
       </div>
     ),
     intents: [
-      <Button.Transaction target="register">Register or claim!</Button.Transaction>
+      (canClaim || !registered) && (
+        <Button.Transaction target="claimAndOrRegister">
+          {canClaim ? "Claim!" : "Register!"}
+        </Button.Transaction>
+      ),
     ],
-  })
-})
+  });
+});
 
-app.frame('/registered', async (c) => {
-  const { transactionId } = c
+app.frame("/registered", async (c) => {
+  const { transactionId } = c;
   return c.res({
     image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 120 }}>
+      <div style={{ color: "white", display: "flex", fontSize: 120 }}>
         Registered successfuly! {transactionId}
       </div>
     ),
-    intents: [
-      <Button.Reset>Done!</Button.Reset>,
-    ]
-  })
-})
+    intents: [<Button.Reset>Done!</Button.Reset>],
+  });
+});
 
- 
-app.transaction('register', (c) => {
+app.transaction("claimAndOrRegister", (c) => {
   if (!c.frameData?.fid) {
-    return //throw error
-
+    return; //throw error
   }
-  return registerUser(c.contract, c.frameData?.fid)
-})
+  return registerUser(c.contract, c.frameData.fid);
+});
 
-app.use('/*', serveStatic({ root: './public' }))
-devtools(app, { serveStatic })
+app.use("/*", serveStatic({ root: "./public" }));
+devtools(app, { serveStatic });
 
-if (typeof Bun !== 'undefined') {
+if (typeof Bun !== "undefined") {
   Bun.serve({
     fetch: app.fetch,
     port: 3000,
-  })
-  console.log('Server is running on port 3000')
+  });
+  console.log("Server is running on port 3000");
 }
--- src/lib/faucet.ts
+++ src/lib/faucet.ts
@@ -1,26 +1,40 @@
-import { createPublicClient, getContract, http } from 'viem'
-import { optimism } from 'viem/chains'
-import { FAUCET_CONTRACT_ADDRESS, OP_CHAIN_ID } from './utils.js'
-import faucetABI from '../abi/faucetABI.json'
- 
-const publicClient = createPublicClient({ 
+import { createPublicClient, getContract, http } from "viem";
+import { optimism } from "viem/chains";
+import { FAUCET_CONTRACT_ADDRESS, OP_CHAIN_ID } from "./utils.js";
+import faucetABI from "../abi/faucetABI.json";
+
+const publicClient = createPublicClient({
   chain: optimism,
-  transport: http()
-})
+  transport: http(),
+});
 
-const contract = getContract({ address: FAUCET_CONTRACT_ADDRESS, abi: faucetABI, client: publicClient })
+const contract = getContract({
+  address: FAUCET_CONTRACT_ADDRESS,
+  abi: faucetABI,
+  client: publicClient,
+});
 
-export const isUserRegistered = async (fid: number) => {
-    const response = await Promise.all([contract.read.getCurrentPeriod(), contract.read.claimer(fid)])
-    return response
+export async function getUserStatus(fid: number) {
+  const [currentPeriod, claimer] = await Promise.all([
+    contract.read.getCurrentPeriod(),
+    contract.read.claimer(fid),
+  ]);
+  return {
+    canClaim: claimer.latestClaimPeriod < currentPeriod,
+    registered: claimer.registeredClaimPeriod === currentPeriod,
+  };
+}
+
+export function getPeriodLength(contract: any) {
+  return contract.read.periodLength();
 }
 
 export function registerUser(contract: any, fid: number) {
-    return contract({
-        abi: faucetABI,
-        chainId: OP_CHAIN_ID,
-        functionName: 'register',
-        args: [fid],
-        to: FAUCET_CONTRACT_ADDRESS, 
-      })
+  return contract({
+    abi: faucetABI,
+    chainId: `eip155:${OP_CHAIN_ID}`,
+    functionName: "register",
+    args: [fid],
+    to: FAUCET_CONTRACT_ADDRESS,
+  });
 }
